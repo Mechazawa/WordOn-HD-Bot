b27dfb580e7c59ca71cf07b1774b4ca708db47b1
shodan <mega@ioexception.at>
2015-10-13 13:53:31 +0200

    add grid code

---
+++ wordonhd/Grid.py
@@ -0,0 +1,28 @@
+class Grid(object):
+    _modifiers = {
+        '1': lambda x: x.score * 2,
+        '2': lambda x: x.score * 3,
+        '4': lambda x: x + 10,
+    }
+
+    def __init__(self, grid):
+        self._grid = grid.split(',')
+        self._grid_mod = tuple(map(lambda x: self._modifiers.get(x, lambda _: _),
+                                   self._grid))
+
+    def score(self, word):
+        score = 0
+        for i in range(len(word)):
+            score += self._grid_mod[i](word.value)
+
+        return score
+
+    def new_wordle(self, word):
+        index = [i for i, l in enumerate(self._grid) if l is '3']
+        wordles = []
+
+        for i in index:
+            if len(word) >= index:
+                wordles.append(word[i])
+
+        return wordles
